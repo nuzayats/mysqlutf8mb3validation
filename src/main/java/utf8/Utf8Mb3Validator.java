ba71985b502d3ef12254b50c92127bbd7bdf3df5
Kohei Nozaki <kohei@apache.org>
2019-06-29 09:35:27 +0900

    better one

--- src/main/java/utf8/Utf8Mb3Validator.java
+++ src/main/java/utf8/Utf8Mb3Validator.java
@@ -16,10 +16,18 @@ class Utf8Mb3Validator {
         // https://dev.mysql.com/doc/refman/5.7/en/charset-unicode-utf8mb3.html
         // https://www.oracle.com/technetwork/java/javase/downloads/supplementary-142654.html?printOnly=1
         // https://stackoverflow.com/q/56800767/3591946
+
+        // taken from https://stackoverflow.com/a/56814133/3591946
         return input
-                .codePoints() // get Unicode code points
-                .filter(codePoint -> Character.charCount(codePoint) > 1) // search for non-BMP characters
-                .mapToObj(codePoint -> new String(Character.toChars(codePoint))) // convert code points into Strings
+                .codePoints()
+                .filter(codePoint -> codePoint >= 0x1_0000) // https://en.wikipedia.org/wiki/UTF-8
+                .mapToObj(codePoint -> new String(Character.toChars(codePoint)))
                 .collect(Collectors.toSet());
+
+//        return input
+//                .codePoints() // get Unicode code points
+//                .filter(codePoint -> Character.charCount(codePoint) > 1) // search for non-BMP characters
+//                .mapToObj(codePoint -> new String(Character.toChars(codePoint))) // convert code points into Strings
+//                .collect(Collectors.toSet());
     }
 }
